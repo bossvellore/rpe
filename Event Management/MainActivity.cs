8688deeef76f6aa3be12bb265f3286ce0ea5e89a
amal <amal@DESKTOP-KSE7E2A>
2017-05-08 14:21:04 +0530

    Add project files.

---
+++ Event Management/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using com.refractored.fab;
+using Android.Content;
+
+namespace Event_Management
+{
+    [Activity(Label = "Events", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        ListView elv;
+        Button nav;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+
+
+            elv = FindViewById<ListView>(Resource.Id.eventListView);
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.AttachToListView(elv);
+
+            fab.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(EventAdd));
+                    StartActivity(intent);
+            };
+        }
+    }
+}
+
